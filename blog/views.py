01fef1526ef13497537010f95d7dd8bc3d2d85e9
king9 <agz97agz97@gmail.com>
2022-03-31 15:24:39 +0800

    database & models

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,14 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Post
 
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 27 2022',
-
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'August 28 2022',
-    }
-   
-]
 
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
